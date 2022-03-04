6d121d64679b7281157b697e56a3893b7bf69842
Blessing Ubogu <blessingubogu@Blessings-MacBook-Air.local>
2022-03-04 20:28:09 +0000

    tweet can be sent

---
+++ twitter_bot.rb
@@ -0,0 +1,11 @@
+require 'Twitter'
+require 'dotenv/load' 
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["API_KEY"]
+  config.consumer_secret     = ENV["API_SECRET_KEY"]
+  config.access_token        = ENV["ACCESS_TOKEN"]
+  config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+end
+
+client.update("When life gives you lemons, make lemonade.”")
